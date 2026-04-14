32aac948a2c9d20b6be0ce88439bb4ab1968cf5e
goddems <artemkaluznij324@gmail.com>
2026-04-14 17:39:25 +0300

    Day 2: CRUD API with validation

--- backend/src/authors/dto/create-author.dto.ts
+++ backend/src/authors/dto/create-author.dto.ts
@@ -1,1 +1,13 @@
-export class CreateAuthorDto {}
+import { IsString, IsEmail, IsOptional } from 'class-validator';
+
+export class CreateAuthorDto {
+  @IsString({ message: 'Name must be a string' })
+  name: string;
+
+  @IsEmail({}, { message: 'Email must be a valid email address' })
+  email: string;
+
+  @IsOptional()
+  @IsString({ message: 'Bio must be a string' })
+  bio?: string;
+}
--- backend/src/books/books.controller.ts
+++ backend/src/books/books.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('books')
 export class BooksController {
@@ -11,8 +21,26 @@ export class BooksController {
     return this.booksService.findAll();
   }
 
+  @Get(':id')
+  getOne(@Param('id', ParseIntPipe) id: number) {
+    return this.booksService.findOne(id);
+  }
+
   @Post()
   create(@Body() createBookDto: CreateBookDto) {
     return this.booksService.create(createBookDto);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateBookDto: UpdateBookDto,
+  ) {
+    return this.booksService.update(id, updateBookDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.booksService.remove(id);
   }
 }
